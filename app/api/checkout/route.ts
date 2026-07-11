3b991442151718c055ca438711b3c9bff50ea1b5
cmarqc <cmqcalloway@gmail.com>
2026-07-11 14:32:20 -0700

    feat: enable promotion codes in Stripe Checkout session

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -57,6 +57,7 @@ export async function POST(request: Request) {
   try {
     const session = await stripe().checkout.sessions.create({
       mode: 'payment',
+      allow_promotion_codes: true,
       line_items: [
         {
           quantity: 1,
